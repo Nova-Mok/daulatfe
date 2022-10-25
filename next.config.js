d4e94be3610e56de94e44eb4a51c2ef03fc17e46
omer <mdomerkhan800@gmail.com>
2022-10-25 06:15:45 +0530

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["egoverse.infura-ipfs.io", "infura-ipfs.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
